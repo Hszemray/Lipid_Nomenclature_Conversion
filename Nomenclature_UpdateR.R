75961e71002cc40501c23d5370a4dd2f3e2779a1
Hszemray <122852247+Hszemray@users.noreply.github.com>
2025-03-19 13:11:56 +0800

    Update Nomenclature_UpdateR.R
    Add counter for updated lipids and list of columns not updated

--- Nomenclature_UpdateR.R
+++ Nomenclature_UpdateR.R
@@ -9,30 +9,41 @@ Nomenclature_update <- function(Data) {
   # Create named vector for nomenclature mapping
   nomenclature_map <- setNames(Nomenclature_key$updated_precursor_name, Nomenclature_key$precursor_name)
   
-    ## Ensure formatting of species is correct
-    Target_Columns <- c("CE", "CER", "MAG", "DAG", "FFA", "LCER", "HCER", "LPC", "LPE", "LPI", "LPG", "LPS", "PE", "PC", "PS", "PG", "PI", "SM", "TAG", "DCER")
-    colnames(Data) <- sapply(colnames(Data), function(Formatting) {
-      if (any(sapply(Target_Columns, function(target) startsWith(Formatting, target)))) {
-        if (grepl("\\.", Formatting)) {
-          Formatting <- gsub("\\.", ":", Formatting)
-          if (startsWith(Formatting, "PE:P") || startsWith(Formatting, "PE:O")) {
-            Formatting <- sub("^PE:P:", "PE:P-", Formatting)
-            Formatting <- sub("^PE:O:", "PE:O-", Formatting)
-          }
-          Formatting <- sub("\\:$", ")", Formatting)
-          Formatting <- sub(":", "(", Formatting, fixed = TRUE)
+  ## Ensure formatting of species is correct
+  Target_Columns <- c("CE", "CER", "MAG", "DAG", "FFA", "LCER", "HCER", "LPC", "LPE", "LPI", "LPG", "LPS", "PE", "PC", "PS", "PG", "PI", "SM", "TAG", "DCER")
+  colnames(Data) <- sapply(colnames(Data), function(Formatting) {
+    if (any(sapply(Target_Columns, function(target) startsWith(Formatting, target)))) {
+      if (grepl("\\.", Formatting)) {
+        Formatting <- gsub("\\.", ":", Formatting)
+        if (startsWith(Formatting, "PE:P") || startsWith(Formatting, "PE:O")) {
+          Formatting <- sub("^PE:P:", "PE:P-", Formatting)
+          Formatting <- sub("^PE:O:", "PE:O-", Formatting)
         }
+        Formatting <- sub("\\:$", ")", Formatting)
+        Formatting <- sub(":", "(", Formatting, fixed = TRUE)
       }
-      return(Formatting)
-    })
-    
+    }
+    return(Formatting)
+  })
+  
+  # Start counters and lists
+  updated_count <- 0
+  not_updated <- list()
+  
   # Update Nomenclature
   colnames(Data) <- sapply(colnames(Data), function(nomenclature) {
     if (nomenclature %in% names(nomenclature_map)) {
+      updated_count <<- updated_count + 1
       return(nomenclature_map[nomenclature]) # replaces old nomenclature with updated nomenclature
     } else {
+      not_updated <<- c(not_updated, nomenclature)
       return(nomenclature) # keeps original nomenclature if no match is found in nomenclature key
     }
   })
+  
+  # Print the results
+  cat("Number of column names updated:", updated_count, "\n")
+  cat("Column names not updated:", paste(not_updated, collapse = ", "), "\n")
+  
   return(Data)
 }
